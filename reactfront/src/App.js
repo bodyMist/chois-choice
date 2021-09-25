bc200ba9b82e23ac979db94cc23b3c5ec6042931
been2 <nrjs3k@gmail.com>
2021-09-26 03:45:50 +0900

    구조변경

--- reactfront/src/App.js
+++ reactfront/src/App.js
@@ -3,13 +3,15 @@ import Header from "./component/Header";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Footer from "./component/Footer";
 import Routing from "./Routing";
+import Image from "./component/Image";
+import BestPost from "./component/BestPost";
 
 function App() {
     return (
         <BrowserRouter>
             <div className="App">
                 <Header />
-                  <Routing/>
+                <Routing/>
                 <Footer/>
             </div>
         </BrowserRouter>
