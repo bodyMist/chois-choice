f0c38a9c51b0c45f9330c88d5d25fd96fb96448d
been2 <nrjs3k@gmail.com>
2021-09-26 03:46:33 +0900

    변경없는데 왜..?

--- reactfront/src/component/MenuList.js
+++ reactfront/src/component/MenuList.js
@@ -3,12 +3,13 @@ import dummy from "../DB/menulist.json";
 
 export default function MenuList() {
     return (
-        <ul className="list">
+        <ul className="menuList">
             {dummy.menus.map((menu) => (
                 <li key={menu.id}>
                     <Link to={`/${menu.id}`}>{menu.menu}</Link>
                 </li>
             ))}
         </ul>
+        
     );
 }
