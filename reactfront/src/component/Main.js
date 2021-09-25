7c4dc7a49fdcccc76fa667f38d0e8254adcc851a
been2 <nrjs3k@gmail.com>
2021-09-26 03:47:13 +0900

    메인화면 컴포넌트 작성

---
+++ reactfront/src/component/Main.js
@@ -0,0 +1,13 @@
+import BestPost from "./BestPost";
+import Image from "./Image";
+import MenuList from "./MenuList";
+
+export default function Main() {
+    return (
+        <>
+            <MenuList></MenuList>
+            <Image></Image>
+            <BestPost></BestPost>
+        </>
+    );
+}
