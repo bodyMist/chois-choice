5ef8095b6e2a69700b46023ebc06c70e7b166d44
been2 <nrjs3k@gmail.com>
2021-09-27 09:12:39 +0900

    구조변경

--- reactfront/src/App.js
+++ reactfront/src/App.js
@@ -1,18 +1,25 @@
-
 import Header from "./component/Header";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Footer from "./component/Footer";
 import Routing from "./Routing";
-import Image from "./component/Image";
-import BestPost from "./component/BestPost";
+import Main from "./component/Main";
+import Estimate from "./mainPage/menu/Estimate";
+import Parts from "./mainPage/menu/Parts";
+import Board from "./mainPage/menu/Board";
+import Share from "./mainPage/menu/Share";
+import Notice from "./mainPage/menu/Notice";
+import QnA from "./mainPage/menu/QnA";
+import Login from "./component/Login";
+import Regist from "./component/Regist";
+import EmptyPage from "./component/EmptyPage";
 
 function App() {
     return (
         <BrowserRouter>
             <div className="App">
-                <Header />
-                <Routing/>
-                <Footer/>
+                <Switch>
+                    <Routing/>
+                </Switch>
             </div>
         </BrowserRouter>
     );
