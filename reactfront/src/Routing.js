91f62f83ea3248ca36e1dba16921bcbeaecaac1a
been2 <nrjs3k@gmail.com>
2021-09-27 09:15:05 +0900

    파일위치 변경으로 인한 import 경로 수정, 로그인 회원가입 route 추가, 로그인, 회원가입 화면에서 헤더, 푸터 보이지 않게 수정

--- reactfront/src/Routing.js
+++ reactfront/src/Routing.js
@@ -1,26 +1,34 @@
 import { Route, Switch } from "react-router-dom";
 import EmptyPage from "./component/EmptyPage";
 
-import Estimate from "./menu/Estimate";
-import Parts from "./menu/Parts";
-import Board from "./menu/Board";
-import Share from "./menu/Share";
-import Notice from "./menu/Notice";
-import QnA from "./menu/QnA";
+import Estimate from "./mainPage/menu/Estimate";
+import Parts from "./mainPage/menu/Parts";
+import Board from "./mainPage/menu/Board";
+import Share from "./mainPage/menu/Share";
+import Notice from "./mainPage/menu/Notice";
+import QnA from "./mainPage/menu/QnA";
 import Main from "./component/Main";
+import Login from "./component/Login";
+import Regist from "./component/Regist";
+import Header from "./component/Header";
+import Footer from "./component/Footer";
 
 export default function Routing() {
     return (
         <Switch>
-            <Route exact path="/" component={Main} />
-            <Route path="/Estimate" component={Estimate} />
-            <Route path="/Parts" component={Parts} />
-            <Route path="/Board" component={Board} />
-            <Route path="/Share" component={Share}/>
-            <Route path="/Notice" component={Notice}/>
-            <Route path="/QnA" component={QnA}/>
-            <Route component={EmptyPage} />
-            
+            <Route path="/Login" component={Login} />
+            <Route path="/Regist" component={Regist} />
+            <>
+                <Header />
+                <Route exact path="/" component={Main} />
+                <Route path="/Estimate" component={Estimate} />
+                <Route path="/Parts" component={Parts} />
+                <Route path="/Board" component={Board} />
+                <Route path="/Share" component={Share} />
+                <Route path="/Notice" component={Notice} />
+                <Route path="/QnA" component={QnA} />
+                <Footer />
+            </>
         </Switch>
     );
 }
