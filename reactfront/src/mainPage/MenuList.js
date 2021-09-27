f942d352fb6f8037a5275ec2a13f8c2728f99978
been2 <nrjs3k@gmail.com>
2021-09-27 09:13:45 +0900

    파일 위치 변경

---
+++ reactfront/src/mainPage/MenuList.js
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+import dummy from "../DB/menulist.json";
+
+export default function MenuList() {
+    return (
+        <ul className="menuList">
+            {dummy.menus.map((menu) => (
+                <li key={menu.id}>
+                    <Link to={`/${menu.id}`}>{menu.menu}</Link>
+                </li>
+            ))}
+        </ul>
+        
+    );
+}
---
+++ reactfront/src/mainPage/menu/Share.js
@@ -0,0 +1,116 @@
+export default function() {
+    return (
+        <div className="articleList">
+            <ul className="list">
+                <li className="articleSubject">
+                    <a className="subject" href="#">
+                        <span className="title">
+                            <strong>공유글 9</strong>
+                        </span>
+                    </a>
+                    <span className="info">
+                        <em className="writer">작성자</em>
+                        &nbsp;|&nbsp;
+                        <span>05:59</span>
+                    </span>
+                </li>
+                <li className="articleSubject">
+                    <a className="subject" href="#">
+                        <span className="title">
+                            <strong>공유글 8</strong>
+                        </span>
+                    </a>
+                    <span className="info">
+                        <em className="writer">작성자</em>
+                        &nbsp;|&nbsp;
+                        <span>04:59</span>
+                    </span>
+                </li>
+                <li className="articleSubject">
+                    <a className="subject" href="#">
+                        <span className="title">
+                            <strong>공유글 7</strong>
+                        </span>
+                    </a>
+                    <span className="info">
+                        <em className="writer">작성자</em>
+                        &nbsp;|&nbsp;
+                        <span>03:59</span>
+                    </span>
+                </li>
+                <li className="articleSubject">
+                    <a className="subject" href="#">
+                        <span className="title">
+                            <strong>공유글 6</strong>
+                        </span>
+                    </a>
+                    <span className="info">
+                        <em className="writer">작성자</em>
+                        &nbsp;|&nbsp;
+                        <span>02:59</span>
+                    </span>
+                </li>
+                <li className="articleSubject">
+                    <a className="subject" href="#">
+                        <span className="title">
+                            <strong>공유글 5</strong>
+                        </span>
+                    </a>
+                    <span className="info">
+                        <em className="writer">작성자</em>
+                        &nbsp;|&nbsp;
+                        <span>01:59</span>
+                    </span>
+                </li>
+                <li className="articleSubject">
+                    <a className="subject" href="#">
+                        <span className="title">
+                            <strong>공유글 4</strong>
+                        </span>
+                    </a>
+                    <span className="info">
+                        <em className="writer">작성자</em>
+                        &nbsp;|&nbsp;
+                        <span>00:59</span>
+                    </span>
+                </li>
+                <li className="articleSubject">
+                    <a className="subject" href="#">
+                        <span className="title">
+                            <strong>공유글 3</strong>
+                        </span>
+                    </a>
+                    <span className="info">
+                        <em className="writer">작성자</em>
+                        &nbsp;|&nbsp;
+                        <span>00:59</span>
+                    </span>
+                </li>
+                <li className="articleSubject">
+                    <a className="subject" href="#">
+                        <span className="title">
+                            <strong>공유글 2</strong>
+                        </span>
+                    </a>
+                    <span className="info">
+                        <em className="writer">작성자</em>
+                        &nbsp;|&nbsp;
+                        <span>21:59</span>
+                    </span>
+                </li>
+                <li className="articleSubject">
+                    <a className="subject" href="#">
+                        <span className="title">
+                            <strong>공유글 1</strong>
+                        </span>
+                    </a>
+                    <span className="info">
+                        <em className="writer">작성자</em>
+                        &nbsp;|&nbsp;
+                        <span>22:59</span>
+                    </span>
+                </li>
+            </ul>
+        </div>
+    );
+}
