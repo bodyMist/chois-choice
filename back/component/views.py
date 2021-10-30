2fe27a13ed473e665c7b0ee1193a959f3bb21810
bodyMist <sky834459@gmail.com>
2021-10-31 03:21:55 +0900

    add basic viewer classes of member and component

--- back/component/views.py
+++ back/component/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import viewsets,generics
+from .serializer import *
+from .models import *
 
 # Create your views here.
+class CpuView(viewsets.ModelViewSet):
+    serializer_class = CpuSerializer
+    queryset = Cpu.objects.all()
+
+class CpuDetail(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = CpuSerializer
+    queryset = Cpu.objects.all()
