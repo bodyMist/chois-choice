b43bc5cbfe1490828573260a83b62bb866261bfd
bodyMist <sky834459@gmail.com>
2021-10-30 09:00:12 +0900

    register component&post model on admin site

--- back/component/admin.py
+++ back/component/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from .models import Component, Case, Cooler, Cpu, Gpu, Hdd, Mainboard, Memory, Power, Ssd
+
 # Register your models here.
+admin.site.register(Component)
+admin.site.register(Cpu)
+admin.site.register(Gpu)
+admin.site.register(Mainboard)
+admin.site.register(Memory)
+admin.site.register(Hdd)
+admin.site.register(Ssd)
+admin.site.register(Power)
+admin.site.register(Cooler)
+admin.site.register(Case)
--- back/post/admin.py
+++ back/post/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Posts, Likes, Reports
 
 # Register your models here.
+admin.site.register(Posts)
+admin.site.register(Likes)
+admin.site.register(Reports)
