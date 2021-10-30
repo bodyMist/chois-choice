0074ca540eb93157afa0cace0c69e5ff59dfb347
been2 <nrjs3k@gmail.com>
2021-10-30 20:36:59 +0900

    create axios get method

--- front/src/component/menu/Estimate/PartSelect.js
+++ front/src/component/menu/Estimate/PartSelect.js
@@ -1,4 +1,21 @@
+import { useState } from "react";
+import axios from "axios";
+
 export default function PartSelect() {
+    const handleClick = (e) => {
+        axios
+            .get("/api")
+            .then(function (response) {
+                console.log(response);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    };
+    // const [result, setResult] = useState("");
+    // axios.get("/api").then((res) => setResult(res.data[0].account));
+    // const request=axios.get("/api").then((response)=>response.data)
+
     return (
         <div className="col-xs-12 container-partSelect">
             <div className="col-xs-12 parts">
@@ -6,50 +23,13 @@ export default function PartSelect() {
                     <div className="input-group">
                         <input type="text" className="form-control" placeholder="부품 명 검색"></input>
                         <div className="input-group-btn">
-                            <button className="btn btn-default" type="button">검색</button>
+                            <button className="btn btn-default" type="button" onClick={handleClick}>검색</button>
                             <button className="btn btn-default" type="button">초기화</button>
                         </div>
                    </div>
                </div>
                <div className="StuffListView">
                    <div className="StuffList">
-                       <div className="StuffIcon">
-                            <img alt="1" src="/1.jpg"/>
-                       </div>
-                       <div className="btn text-left">
-                            <span>CPU : </span>
-                            <span className="label label-info">추천</span>
-                            <span>AMD 3600 (정품)</span>
-                       </div>
-                   </div>
-                   <div className="StuffList">
-                       <div className="StuffIcon">
-                            <img alt="1" src="/1.jpg"/>
-                       </div>
-                       <div className="btn text-left">
-                            <span>CPU : </span>
-                            <span className="label label-info">추천</span>
-                            <span>AMD 3600 (정품)</span>
-                       </div>
-                   </div><div className="StuffList">
-                       <div className="StuffIcon">
-                            <img alt="1" src="/1.jpg"/>
-                       </div>
-                       <div className="btn text-left">
-                            <span>CPU : </span>
-                            <span className="label label-info">추천</span>
-                            <span>AMD 3600 (정품)</span>
-                       </div>
-                   </div><div className="StuffList">
-                       <div className="StuffIcon">
-                            <img alt="1" src="/1.jpg"/>
-                       </div>
-                       <div className="btn text-left">
-                            <span>CPU : </span>
-                            <span className="label label-info">추천</span>
-                            <span>AMD 3600 (정품)</span>
-                       </div>
-                   </div><div className="StuffList">
                        <div className="StuffIcon">
                             <img alt="1" src="/1.jpg"/>
                        </div>
