2b564ec753941f92c2efaca89e1da9dbd9a1bd36
bodyMist <sky834459@gmail.com>
2021-10-30 19:05:38 +0900

    comment CORS_ORIGIN_WHITELIST due to error

--- back/back/settings.py
+++ back/back/settings.py
@@ -53,6 +53,7 @@ REST_FRAMEWORK = {
 }
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -60,10 +61,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 CORS_ORIGIN_WHITELIST = [
+    # 'https://bodymist.github.io/chois-choice',
     'http://localhost:3000',
 ]
 
