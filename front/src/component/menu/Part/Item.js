5e8b855281716060558624907f7c248464f203a1
been2 <nrjs3k@gmail.com>
2021-11-06 07:26:58 +0900

    부품 화면 구현

--- front/src/component/menu/Part/Item.js
+++ front/src/component/menu/Part/Item.js
@@ -1,62 +1,23 @@
 import { useState } from "react";
 
-export default function Item({ inf }) {
-  const [isClicked, setIsClicked] = useState(false);
-  const click = () => {
-    setIsClicked(!isClicked);
-  }
-  const moreInfo = {
-    display: "inline-block",
-  };
-  const lessInfo = {
-    display: "none",
-  }
-  let index = 0;
+export default function Item({ key, title, checkedItemHandler, id }) {
+    const [bChecked, setChecked] = useState(false);
+    const checkedHandler = (e) => {
+        setChecked(!bChecked);
+        const title = e.target.title
+        const id = e.target.id
+        const inf = {title, id};
+
+        checkedItemHandler(inf, e.target.checked);
+    }
     return (
-        <dd className="item_dd">
-            <ul className="item_list item_list_popular">
-                {inf.map((infData, i) => {
-                    if (i < 5) {
-                        return (
-                            <li className="sub_item" key={infData.name}>
-                                <label title={infData.name}>
-                                    <input
-                                        type="checkbox"
-                                        title={infData.name}
-                                    />
-                                    &nbsp;{infData.name}&nbsp;
-                                </label>
-                            </li>
-                        );
-                    }
-                })}
-            </ul>
-            <ul className="item_list item_list_all" style={isClicked ? moreInfo : lessInfo}>
-                {inf.map((infData, i) => {
-                  index = i;
-                    if (i >= 5) {
-                        return (
-                            <li className="sub_item" key={infData.name}>
-                                <label title={infData.name}>
-                                    <input
-                                        type="checkbox"
-                                        title={infData.name}
-                                    />
-                                    &nbsp;{infData.name}&nbsp;
-                                </label>
-                            </li>
-                        );
-                    }
-                })}
-            </ul>
-            <div className="spec_opt_view">
-                <button type="button" className="btn_spec_view btn_view_more" style={isClicked ? lessInfo : moreInfo} onClick={click}>
-                    <strong>{index-4}</strong>개
-                </button>
-                <button type="button" className="btn_spec_view btn_view_less" style={isClicked ? moreInfo : lessInfo} onClick={click}>
-                    <strong>닫기</strong>
-                </button>
-            </div>
-        </dd>
+        <>
+            <li className="sub_item" key={key}>
+                <label title={title}>
+                    <input type="checkbox" title={title} checked={bChecked} onChange={checkedHandler} id={id}/>
+                    &nbsp;{title}&nbsp;
+                </label>
+            </li>
+        </>
     );
 }
