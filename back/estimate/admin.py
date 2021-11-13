a2afb0377248f4a5305b17aede81b202fc404765
bodyMist <sky834459@gmail.com>
2021-11-14 04:09:19 +0900

    put all models of esitmate module in admin site

--- back/estimate/admin.py
+++ back/estimate/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+admin.site.register(Estimation)
+admin.site.register(Evaluations)
+admin.site.register(EstimationHasComponent)
+admin.site.register(Annotations)
+admin.site.register(Uses)
